bc577d18fa19dc071842b703623dd4981479f680
ssong-eu <sangyoon.song@gmail.com>
2019-01-23 15:08:31 +0000

    added private / unprivate, delete, find forked functionality

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ const username = '' // Your github username
 const accessToken = '' // Your github access token
 const orgName = '' // The organisation name the repos should be transferred to. Create organisation manually first.
 const searchTerm = `` // The search term for the repos you want to transfer
+const forked = false // Set to true if you want to find only forked repos
+const option = 'transfer' // Set to either 'delete', 'private', 'public', or 'transfer'
 
 // Gets the first 100 repos, may need to run a few times.
 const getRepos = () => {
@@ -24,9 +26,27 @@ const getRepos = () => {
     }
   }).then(res => res.json())
     .then(data => data.map(repo =>
-      repo.name.includes(`${searchTerm}`) ? repoList.push(repo.name) : null
+      (repo.name.includes(`${searchTerm}`)) && (forked ? repo.fork === true : null) ? repoList.push(repo.name) : null
     ))
-    .then(() => repoList.forEach(repo => moveRepo(repo)))
+    .then(() => repoList.forEach(repo => {
+      switch (option) {
+        case 'transfer':
+          moveRepo(repo)
+          break
+        case 'delete':
+          deleteRepo(repo)
+          break
+        case 'private':
+          hideRepo(repo)
+          break
+        case 'public':
+          unhideRepo(repo)
+          break
+        default:
+          console.log('incorrect option selection')
+          break
+      }
+    }))
 }
 
 // Used by getRepos to transfer the ownership.
@@ -42,7 +62,48 @@ const moveRepo = (repoName) => {
     })
   })
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => data.message ? console.log(`${repoName}: ${data.message}`) : console.log(`${repoName} transferred`))
+}
+
+// Used by getRepos to make the repo private
+const hideRepo = (repoName) => {
+  fetch(`https://api.github.com/repos/${username}/${repoName}`, {
+    method: 'PATCH',
+    headers: {
+      Authorization: `token ${accessToken}`
+    },
+    body: JSON.stringify({
+      private: true
+    })
+  })
+    .then(res => res.json())
+    .then(data => data.message ? console.log(`${repoName}: ${data.message}`) : console.log(`${repoName} hidden`))
+}
+
+// Used by getRepos to make the repo public
+const unhideRepo = (repoName) => {
+  fetch(`https://api.github.com/repos/${username}/${repoName}`, {
+    method: 'PATCH',
+    headers: {
+      Authorization: `token ${accessToken}`
+    },
+    body: JSON.stringify({
+      private: false
+    })
+  })
+    .then(res => res.json())
+    .then(data => data.message ? console.log(`${repoName}: ${data.message}`) : console.log(`${repoName} unhidden`))
+}
+
+// Used by getRepos to delete the repo
+const deleteRepo = (repoName) => {
+  fetch(`https://api.github.com/repos/${username}/${repoName}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `token ${accessToken}`
+    }
+  })
+    .then(() => console.log(`${repoName} deleted`))
 }
 
 // RUN!
